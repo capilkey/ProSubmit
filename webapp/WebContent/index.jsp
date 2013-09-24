06dbb54a6db30511574e2278fa62a3a3c0fa4522
Ramone Burrell <burrellramone@gmail.com>
2013-09-24 11:22:44 -0500

    Committing login page design as well as the addition of an empty authentication HTTP servlet

---
+++ webapp/WebContent/index.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Login</title>
+<link type="text/css" rel="stylesheet" href="/ProSubmit/resources/lib/bootstrap/css/bootstrap.css"> 
+<link type="text/css" rel="stylesheet" href="/ProSubmit/resources/css/prosubmit.css"> 
+<link type="text/css" rel="stylesheet" href="/ProSubmit/resources/css/login.css">
+
+<script type="text/javascript" src="/ProSubmit/resources/js/jquery.js"></script>
+<script type="text/javascript" src="/ProSubmit/resources/js/prosubmit.js"></script>
+<script type="text/javascript" src="/ProSubmit/resources/lib/bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+		<form id="loginForm">
+			<h2>ProSubmit Login</h2>
+			<label for="username">Username:</label>
+			<input type="text" id="username" placeHolder="Username" class="form-control"/>
+			<label for="password">Password:</label>
+			<input type="text" id="password" placeHolder="Password" class="form-control"/><br/><br/>
+			<button type="button" class="btn btn-primary" onclick="return proSubmit.login()">Login</button>
+			<br/><br/>
+		</form>
+</body>
+</html>
---
+++ webapp/src/prosubmit/servlet/AuthenticateServlet.java
@@ -0,0 +1,55 @@
+package prosubmit.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author ramone
+ * Servlet implementation class AuthenticateServlet
+ */
+@SuppressWarnings("all")
+public final class AuthenticateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AuthenticateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
