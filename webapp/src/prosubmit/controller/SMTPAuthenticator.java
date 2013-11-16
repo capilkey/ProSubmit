8551edacc2ed19e35f288ee6c436c2bba098d164
Ramone Burrell <burrellramone@gmail.com>
2013-11-15 21:16:13 -0500

    Update SMTPAuthenticator.java
    Edited out password

--- webapp/src/prosubmit/controller/SMTPAuthenticator.java
+++ webapp/src/prosubmit/controller/SMTPAuthenticator.java
@@ -23,7 +23,7 @@ public class SMTPAuthenticator extends Authenticator {
 	@Override
 	public PasswordAuthentication getPasswordAuthentication() {
 	 String username = "raburrell";
-	 String password = "d@*L2&9";
+	 String password = "*******";
 	    if ((username != null) && (username.length() > 0) && (password != null) 
 	      && (password.length   () > 0)) {
 	        return new PasswordAuthentication(username, password);
